0620f3b1978fda35eb236557d2f27c63ba0c2991
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2025-05-10 11:54:33 +0300

    imgPaps

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,12 @@
+import css from "./Hero.module.css"
+
 const Hero = () => {
-    return <section></section>
+	return (
+		<section className={css.sectionHero}>
+			<div className={css.cardOne}><p>awd</p></div>
+			<div className={css.cardTwo}><p>awd</p></div>
+		</section>
+	)
 }
 
-export default Hero;
+export default Hero
