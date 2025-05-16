1fa5f1b1da744bfe514903b093582b5ef67a3546
Devohka <andr.fedak@gmail.com>
2025-05-16 22:44:11 +0300

    gh

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -1,5 +1,141 @@
+import css from "./Burger.module.css";
+
 const Burger = () => {
-  return <div></div>;
+  return (
+    <div className={css.burger_modal}>
+      <ul className={css.burger_list}>
+        <li className={css.burger__list_item}>
+          <a className={css.burger__item_page}>Artist</a>
+        </li>
+        <li className={css.burger__list_item}>
+          <a className={css.burger__item_page}>The form</a>
+        </li>
+        <li className={css.burger__list_item}>
+          <a className={css.burger__item_page}>Reviews</a>
+        </li>
+      </ul>
+
+      <ul className={css.burger_contact_list}>
+        <li className={css.burger_contact__list_item}>
+          <a href="https://www.facebook.com/goITclub/">
+            <svg
+              width="30.000000"
+              height="30.000000"
+              viewBox="0 0 30 30"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect
+                id="Telegram"
+                rx="14.500000"
+                width="29.000000"
+                height="29.000000"
+                transform="translate(0.500000 0.500001)"
+                fill="#191919"
+                fill-opacity="1.000000"
+              />
+              <g clip-path="url(#clip45_10497)">
+                <g clip-path="url(#clip68_456)">
+                  <path
+                    id="Vector"
+                    d="M16.54 10.03C16.64 10.03 16.75 10.03 16.95 10.03L16.95 11.6C16.85 11.6 16.64 11.6 16.43 11.6C16.22 11.6 16.01 11.6 15.91 11.71C15.8 11.81 15.8 11.92 15.8 12.16C15.8 12.37 15.8 12.51 15.8 12.72L16.95 12.72L16.95 14.05L15.8 14.05L15.8 19.83L14.02 19.83L14.02 14.02L13.07 14.02L13.07 12.69L14.02 12.69L14.02 12.23C14.02 11.46 14.12 10.9 14.33 10.66C14.65 10.2 15.28 9.99 16.22 9.99C16.33 10.03 16.43 10.03 16.54 10.03Z"
+                    fill="#FDFF61"
+                    fill-opacity="1.000000"
+                    fill-rule="nonzero"
+                  />
+                </g>
+              </g>
+            </svg>
+          </a>
+        </li>
+        <li className={css.burger_contact__list_item}>
+          <a href="https://www.instagram.com/goitclub/">
+            <svg
+              width="30.000000"
+              height="30.000000"
+              viewBox="0 0 30 30"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect
+                id="instagram"
+                rx="14.500000"
+                width="29.000000"
+                height="29.000000"
+                transform="translate(0.500000 0.500001)"
+                fill="#191919"
+                fill-opacity="1.000000"
+              />
+              <rect
+                id="icons / 40x40 / instagram"
+                rx="0.000000"
+                width="13.000000"
+                height="13.000000"
+                transform="translate(8.500000 8.500001)"
+                fill="#FFFFFF"
+                fill-opacity="0"
+              />
+              <g clip-path="url(#clip68_458)">
+                <path
+                  id="Vector"
+                  d="M20.56 12.7C20.55 12.23 20.47 11.76 20.3 11.32C20.01 10.57 19.41 9.98 18.66 9.69C18.23 9.53 17.77 9.44 17.3 9.43C16.7 9.4 16.51 9.39 15 9.39C13.48 9.39 13.28 9.39 12.69 9.43C12.22 9.44 11.76 9.53 11.33 9.69C10.58 9.98 9.98 10.57 9.69 11.32C9.53 11.76 9.44 12.22 9.43 12.68C9.4 13.28 9.4 13.47 9.4 14.99C9.4 16.5 9.4 16.7 9.43 17.29C9.44 17.75 9.53 18.21 9.69 18.65C9.98 19.4 10.58 19.99 11.33 20.28C11.76 20.45 12.22 20.55 12.69 20.56C13.29 20.59 13.48 20.6 15 20.6C16.51 20.6 16.71 20.6 17.3 20.56C17.77 20.55 18.23 20.46 18.66 20.3C19.41 20.01 20.01 19.42 20.3 18.67C20.46 18.23 20.55 17.77 20.56 17.31C20.59 16.71 20.6 16.52 20.6 15C20.59 13.49 20.59 13.29 20.56 12.7ZM14.99 17.85C13.4 17.85 12.12 16.57 12.12 14.98C12.12 13.4 13.4 12.11 14.99 12.11C16.58 12.11 17.87 13.4 17.87 14.98C17.87 16.57 16.58 17.85 14.99 17.85ZM17.98 12.67C17.61 12.67 17.31 12.38 17.31 12.01C17.31 11.64 17.61 11.34 17.98 11.34C18.35 11.34 18.65 11.64 18.65 12.01C18.65 12.38 18.35 12.67 17.98 12.67Z"
+                  fill="#FDFF61"
+                  fill-opacity="1.000000"
+                  fill-rule="nonzero"
+                />
+                <path
+                  id="Vector"
+                  d="M16.74 14.98C16.74 15.95 15.96 16.73 14.99 16.73C14.02 16.73 13.24 15.95 13.24 14.98C13.24 14.02 14.02 13.23 14.99 13.23C15.96 13.23 16.74 14.02 16.74 14.98Z"
+                  fill="#FDFF61"
+                  fill-opacity="1.000000"
+                  fill-rule="evenodd"
+                />
+              </g>
+            </svg>
+          </a>
+        </li>
+        <li className={css.burger_contact__list_item}>
+          <a href="https://www.youtube.com/c/GoIT">
+            <svg
+              width="30.000000"
+              height="30.000000"
+              viewBox="0 0 30 30"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect
+                id="YouTube"
+                rx="14.500000"
+                width="29.000000"
+                height="29.000000"
+                transform="translate(0.500000 0.500001)"
+                fill="#191919"
+                fill-opacity="1.000000"
+              />
+              <rect
+                id="ei:sc-youtube"
+                rx="0.000000"
+                width="13.000000"
+                height="13.000000"
+                transform="translate(8.500000 8.500001)"
+                fill="#FFFFFF"
+                fill-opacity="0"
+              />
+              <g clip-path="url(#clip45_10504)">
+                <path
+                  id="Vector"
+                  d="M21.17 12.09C21.17 12.09 21.04 11.18 20.66 10.79C20.2 10.27 19.66 10.27 19.41 10.22C17.64 10.1 15 10.1 15 10.1C15 10.1 12.35 10.1 10.58 10.22C10.33 10.27 9.79 10.27 9.33 10.79C8.95 11.18 8.82 12.09 8.82 12.09C8.82 12.09 8.7 13.13 8.7 14.17L8.7 15.12C8.7 16.16 8.82 17.25 8.82 17.25C8.82 17.25 8.95 18.16 9.33 18.54C9.79 19.07 10.42 19.07 10.71 19.11C11.72 19.15 15 19.2 15 19.2C15 19.2 17.64 19.2 19.41 19.07C19.66 19.02 20.2 19.02 20.66 18.5C21.04 18.11 21.17 17.2 21.17 17.2C21.17 17.2 21.29 16.16 21.29 15.12L21.29 14.17C21.29 13.13 21.17 12.09 21.17 12.09ZM13.69 16.34L13.69 12.7L17.1 14.52L13.69 16.34Z"
+                  fill="#FDFF61"
+                  fill-opacity="1.000000"
+                  fill-rule="nonzero"
+                />
+              </g>
+            </svg>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Burger;
