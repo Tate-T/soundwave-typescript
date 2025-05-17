5ddecfa86771671974ea5d0b39274f6fc221f637
m1xture <misha.ivanov.1050@gmail.com>
2025-05-17 12:02:55 +0300

    phone start

---
+++ src/components/Phone/Phone.tsx
@@ -0,0 +1,14 @@
+import Container from "../Common/Container/Container";
+import css from "./Phone.module.css";
+import PhoneBg from "../../images/banners/phone2.png";
+
+const Phone = () => {
+  return (
+    <section className={css.phone}>
+      <Container>
+        <p></p>
+      </Container>
+    </section>
+  );
+};
+export default Phone;
