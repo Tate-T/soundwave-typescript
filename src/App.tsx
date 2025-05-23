eed7b66bebc88cfb02c51685eae1a5c88e837f31
m1xture <misha.ivanov.1050@gmail.com>
2025-05-23 13:27:31 +0300

    banners section fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
+import "./reset.css";
 import "./common.css";
-import ArtistsInfo from "./components/ArtistInfo/ArtistInfo";
 import Artists from "./components/Artists/Artists";
-import Banners from "./components/Banners/Banners";
+import Banners from "./components/Banner/Banner";
 import Feedback from "./components/Feedback/Feedback";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Phone from "./components/Phone/Phone";
 import Reviews from "./components/Reviews/Reviews";
-import "./reset.css";
 
 function App() {
   return (
