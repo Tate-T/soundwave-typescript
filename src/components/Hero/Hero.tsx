1a881ddfa21c920363ffb200fdf34a350c6c3d37
0qsenxx <semen.antipyuk@gmail.com>
2025-05-23 19:57:02 +0300

    player

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,57 +3,15 @@ import { Play, Pause } from "lucide-react";
 import css from "./Hero.module.css";
 import popSrar from "../../images/Hero/girlPopStar.png";
 // import song from '../../../public/song.mp3';
-import song from '../../audio/song.mp3';
+import song from "../../audio/song.mp3";
 
 const Hero = () => {
-  // const audioRef = useRef<HTMLAudioElement>(null)
-  // const [isPlaying, setIsPlaying] = useState(false)
-  // const [duration, setDuration] = useState(0)
-  // const [currentTime, setCurrentTime] = useState(0)
-
-  // const togglePlay = () => {
-  // 	const audio = audioRef.current
-  // 	if (!audio) return
-
-  // 	if (isPlaying) {
-  // 		audio.pause()
-  // 	} else {
-  // 		audio.play().catch(e => {
-  // 			console.error("Playback failed:", e)
-  // 		})
-  // 	}
-  // 	setIsPlaying(!isPlaying)
-  // }
-
-  // const handleTimeUpdate = () => {
-  // 	const audio = audioRef.current
-  // 	if (audio) {
-  // 		setCurrentTime(audio.currentTime)
-  // 	}
-  // }
-
-  // const handleLoadedMetadata = () => {
-  // 	const audio = audioRef.current
-  // 	if (audio) {
-  // 		setDuration(audio.duration)
-  // 	}
-  // }
-
-  // const handleSeek = (e: React.ChangeEvent<HTMLInputElement>) => {
-  // 	const audio = audioRef.current
-  // 	if (audio) {
-  // 		audio.currentTime = Number(e.target.value)
-  // 		setCurrentTime(Number(e.target.value))
-  // 	}
-  // }
-
-  // const formatTime = (time: number) => {
-  // 	const minutes = Math.floor(time / 60)
-  // 	const seconds = Math.floor(time % 60)
-  // 	return `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`
-  // }
-
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [duration, setDuration] = useState<string>();
+  const [currentTime, setCurrentTime] = useState<number>();
+  const [seekBarProgress, setSeekBarProgress] = useState<number>(0.0);
   const audioRef = useRef<HTMLAudioElement>(null);
+  const seekBarRef = useRef<HTMLInputElement>(null);
 
   function formatTime(seconds: number) {
     const minutes = Math.floor(seconds / 60);
@@ -67,9 +25,11 @@ const Hero = () => {
 
     if (audio.paused) {
       audio.play();
+      setIsPlaying(true);
       //   playPauseBtn.textContent = "⏸️";
     } else {
       audio.pause();
+      setIsPlaying(false);
       //   playPauseBtn.textContent = "▶️";
     }
   };
@@ -98,16 +58,79 @@ const Hero = () => {
       </div>
       <div className={css.cardTwo}>
         <div className={css.audioPlayer}>
-          <audio src={song} ref={audioRef} preload="metadata" />
+          <audio
+            src={song}
+            ref={audioRef}
+            preload="metadata"
+            onLoadedMetadata={(evt) => {
+              const duration = audioRef.current?.duration;
+              if (typeof duration === "number") {
+                // console.log(formatTime(duration));
+                setDuration(formatTime(duration));
+              }
+            }}
+            onTimeUpdate={() => {
+              const audio = audioRef.current;
+              if (audio && audio.duration) {
+                setCurrentTime(audio.currentTime);
+                setSeekBarProgress((audio.currentTime / audio.duration) * 100);
+              }
+            }}
+          />
+          <input
+            type="range"
+            id="seekBar"
+            value={seekBarProgress}
+            className={css.seekBar}
+            onInput={(evt) => {
+              const audio = audioRef.current;
+              const target = evt.target as HTMLInputElement;
+
+              if (audio && audio.duration) {
+                const value = parseFloat(target.value);
+                const newTime = (value / 100) * audio.duration;
+                audio.currentTime = newTime;
+                setSeekBarProgress(value);
+              }
+            }}
+          />
+          <div className={css.timeTextsBox}>
+            <span id="currentTime" className={css.currentTimeSpan}>
+              {formatTime(Number(seekBarProgress))}
+            </span>
+            <span id="duration" className={css.durationSpan}>
+              {duration}
+            </span>
+          </div>
           <button
             type="button"
             className={css.playPauseBtn}
             onClick={handlePlayPause}
           >
-            <Play />
-            <input type="range" id="seekBar" value="0" />
-            <span id="currentTime">0:00</span>
-            <span id="duration">4:38</span>
+            {isPlaying ? (
+              <svg
+                width="30"
+                height="30"
+                viewBox="0 0 64 64"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="32" cy="32" r="32" fill="white" />
+                <rect x="24" y="24" width="16" height="16" fill="black" />
+              </svg>
+            ) : (
+              <svg
+                width="30"
+                height="30"
+                viewBox="0 0 64 64"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="32" cy="32" r="32" fill="white" />
+                <polygon points="25,20 25,44 45,32" fill="black" />
+              </svg>
+            )}
+            {/* <Play /> */}
           </button>
         </div>
         {/* <div className={css.audioPlayer}>
